2636655e016814f611c85aff00f7140c6633e37c
kumarkartik3011 <kumarkartik3011@gmail.com>
2022-03-25 15:56:54 +0530

    redux store, chakra and react-router-dom wrapped outside app.jsx and index.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
+import React from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import RegisterScreen from './Screens/RegisterScreen'
+import HomeScreen from './Screens/HomeScreen'
+import ChatScreen from './Screens/ChatScreen'
+import Notfound from './components/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/register" component={RegisterScreen}/>
+                <Route exact path="/" component={HomeScreen}/>
+                <Route exact path="/chats" component={ChatScreen}/>
+                <Route component={Notfound}/>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store'
 import reportWebVitals from './reportWebVitals';
+import {ChakraProvider} from '@chakra-ui/react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+ReactDOM.render (
+    <Provider store={store}>
+      <ChakraProvider>
+        <App/>
+      </ChakraProvider>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
