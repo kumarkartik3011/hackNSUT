0155251e88d01067404f37f2d90e9bbae1d907d6
kumarkartik3011 <kumarkartik3011@gmail.com>
2022-03-25 16:08:11 +0530

    reducers added to store

--- client/src/store.js
+++ client/src/store.js
@@ -3,7 +3,16 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
 const reducers = combineReducers({
-  
+    userRegister: userRegisterReducer,
+    userLogin: userLoginReducer,
+    userSearch: userSearchReducer,
+    chatCurrentSet: chatCurrentSetReducer,
+    chatAllList: chatAllListReducer,
+    chatOneToOneCreate: chatOneToOneCreateReducer,
+    chatOneToOneList: chatOneToOneListReducer,
+    chatGroupCreate: chatGroupCreateReducer,
+    messageListAll: messageListAllReducer,
+    messageSend: messageSendReducer
 })
 
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
